295b61e2ae5c4d515e0844847ac76c2b4f13123d
Grégoire Paris <postmaster@greg0ire.fr>
2021-10-17 15:46:34 +0200

    Drop line about loading phpactor for php
    Phpactor has been made non-optional

--- init.lua
+++ init.lua
@@ -56,13 +56,6 @@ command! -bang PackagerUpdate call PackagerInit() | call packager#update({ 'forc
 command! PackagerClean call PackagerInit() | call packager#clean()
 command! PackagerStatus call PackagerInit() | call packager#status()
 
-"Load plugins only for specific filetype
-augroup packager_filetype
-  autocmd!
-  autocmd FileType php packadd phpactor
-augroup END
-
-
 " add some color
 syntax on
 
