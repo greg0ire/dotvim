521bc84c6561269daa12637f1b4bd541ed3b5b9c
Grégoire Paris <postmaster@greg0ire.fr>
2021-11-08 08:40:25 +0100

    Remove code block related to unused plugin
    luasnip isn't even defined.

--- lua/greg0ire/intellisense.lua
+++ lua/greg0ire/intellisense.lua
@@ -20,8 +20,6 @@ cmp.setup {
     ['<Tab>'] = function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
       else
         fallback()
       end
@@ -29,8 +27,6 @@ cmp.setup {
     ['<S-Tab>'] = function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
       else
         fallback()
       end
