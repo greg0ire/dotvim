f352625f9752d7485ba347a316106c2cd1744877
Grégoire Paris <postmaster@greg0ire.fr>
2022-06-11 11:19:21 +0200

    Sort lua imports

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
-require('greg0ire.general')
-require('greg0ire.plugins')
-require('greg0ire.mappings')
 require('greg0ire.commands')
+require('greg0ire.general')
 require('greg0ire.intellisense')
+require('greg0ire.mappings')
+require('greg0ire.plugins')
 require('greg0ire.telescope')
 require('greg0ire.treesitter')
 vim.cmd 'source ~/.config/nvim/not-migrated-yet.vim'
