673176b103e61c61aef235a42df7ce4ec87972ed
Grégoire Paris <gregoire.paris@manomano.com>
2022-07-22 17:46:26 +0200

    Add shortcut to find lsp references

--- lua/greg0ire/mappings.lua
+++ lua/greg0ire/mappings.lua
@@ -37,6 +37,7 @@ map('n', '<leader>b', ":lua require('telescope.builtin').buffers()<CR>", silentn
 map('n', '<leader>ta', ":lua require('telescope.builtin').lsp_workspace_symbols()<CR>", silentnoremap)
 map('n', '<leader>co', ":lua require('telescope.builtin').commands()<CR>", silentnoremap)
 map('n', '<leader>ca', ":lua vim.lsp.buf.code_action()<CR>", silentnoremap)
+map('n', '<leader>tr', ":lua require('telescope.builtin').lsp_references()<CR>", silentnoremap)
 
 -- search for word under cursor
 map('n', '<leader>w', ':grep <cword><CR>', silentnoremap)
