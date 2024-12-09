30618f433b54ce4dd8eabc5b739d100ae360f4e8
Grégoire Paris <gregoire.paris@manomano.com>
2024-12-09 09:24:11 +0100

    Use phpunit from Inside docker

--- after/ftplugin/php.lua
+++ after/ftplugin/php.lua
@@ -23,3 +23,7 @@ map(0, 'n', '<Leader>te', ':TestFile --testdox<CR>', silentnoremap)
 -- php-indent options
 vim.g.PHP_noArrowMatching = true
 vim.g.PHP_vintage_case_default_indent = 1
+
+if file_exists('docker-compose.yaml') then
+  vim.g['test#php#phpunit#executable'] = 'docker-compose exec php vendor/bin/phpunit'
+end
