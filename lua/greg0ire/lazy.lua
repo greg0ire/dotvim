686e6bd7b455431d6d2bdc69eba68a9d5455d5d0
Grégoire Paris <postmaster@greg0ire.fr>
2025-05-04 16:34:31 +0200

    Switch back to https

--- lua/greg0ire/lazy.lua
+++ lua/greg0ire/lazy.lua
@@ -1,7 +1,7 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "git@github.com:folke/lazy.nvim.git"
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
