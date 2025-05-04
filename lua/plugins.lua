4621acc956f23d79b0b0da3ba44014b475ba9bc2
Grégoire Paris <postmaster@greg0ire.fr>
2025-05-04 16:21:31 +0200

    Try out noice.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,6 +5,21 @@ return {
   "chr4/nginx.vim",
   "fpob/nette.vim",
   {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  },
+  {
     "gbprod/none-ls-php.nvim",
     ft = "php",
   },
